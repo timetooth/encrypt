6b0c77a1a29a97dd110114a22e29458b4aca04ee
timetooth <95702544+timetooth@users.noreply.github.com>
2025-11-29 23:28:34 +0530

    unet no skip fixed

--- models/unet_no_skip.py
+++ models/unet_no_skip.py
@@ -99,7 +99,7 @@ if __name__ == "__main__":
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"Using device: {device}")
 
-    model = UNetNoSkip(in_channels=1, base_channels=64).to(device)
+    model = UNet(in_channels=1, base_channels=64).to(device)
     x = torch.randn((34, 1, 512, 512), device=device)
     print(f"Input shape: {x.shape}")
 
